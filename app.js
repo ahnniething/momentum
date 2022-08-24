8f1f1c6cfe3f747b3fc5d795875438c2208c50c7
ahnniething <ahnniething@gmail.com>
2022-08-24 16:54:25 +0900

    feat: add a login form

--- app.js
+++ app.js
@@ -1,55 +1,14 @@
-const h1 = document.querySelector(".hello:first-child h1");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-console.dir(h1);
-
-function handleTitleClick(){
-    //way 1
-    h1.classList.toggle("clicked");
-
-    //way 2
-    // const clickedClass = "clicked";
-    // if(h1.classList.contains(clickedClass)){
-    //     h1.classList.remove(clickedClass);
-    // }else{
-    //     h1.classList.add(clickedClass);
-    // }
+function onLoginBtnClick() {
+    const username = loginInput.value;
+    console.log(username);
+    // if(username === ""){
+    //     alert("Please write your name.")
+    //  }else if(username.length > 15){
+    //     alert(" Your name is too long.")
+    //  }
 }
 
-function handleTitleClick(){
-    const currentColor = h1.style.color;
-    h1.style.color = currentColor === "blue" ?  "tomato" :  "blue";
-}
-
-function handleMouseEnter(){
-    h1.innerText = "Mouse is here!"
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse is gone!"
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "pink"
-}
-
-function handleWindowCopy(){
-    alert("copier!")
-}
-
-function handleWindowOffline(){
-    alert("SOS NO WIFI!")
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOD!")
-}
-
-
-h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize)
-window.addEventListener("copy", handleWindowCopy)
-window.addEventListener("offline", handleWindowOffline)
-window.addEventListener("online", handleWindowOnline)
+loginButton.addEventListener("click", onLoginBtnClick);
